001203d89a5ff85f1f35c8d9e871b474b444f1aa
john-la-usfca-98 <pkla@dons.usfca.edu>
2026-06-23 04:38:39 -0500

    chore(driver): harden main error handling
    Ensure Driver prints user-friendly messages on unexpected failures and always shuts down its work queue without leaking exceptions.

--- src/main/java/com/cse/cli/Driver.java
+++ src/main/java/com/cse/cli/Driver.java
@@ -47,94 +47,106 @@ public class Driver {
 	 * @param args flag/value pairs used to start this program
 	 */
 	public static void main(String[] args) {
-		ArgumentParser parser = new ArgumentParser(args);
-		boolean partial = parser.hasFlag("-partial");
-		int serverPort = parseServerPort(parser);
+		WorkQueue queue = null;
 
-		int threads = parser.getInteger("-threads", 5);
-		if (threads <= 0) {
-			threads = 5;
-		}
+		try {
+			ArgumentParser parser = new ArgumentParser(args);
+			boolean partial = parser.hasFlag("-partial");
+			int serverPort = parseServerPort(parser);
 
-		ThreadSafeInvertedIndex index = new ThreadSafeInvertedIndex();
-		WorkQueue queue = new WorkQueue(threads);
-		Searcher searcher = new ThreadedSearchProcessor(index, queue);
-
-		if (parser.hasFlag("-text") && parser.hasValue("-text")) {
-			Path path = parser.getPath("-text", Path.of(""));
-			try {
-				ThreadFileIndexer.indexPath(path, index, queue);
-			} catch (IOException e) {
-				System.out.println("Unable to index text file: " + path);
+			int threads = parser.getInteger("-threads", 5);
+			if (threads <= 0) {
+				threads = 5;
 			}
-		}
 
-		if (parser.hasFlag("-html")) {
-			String seed = parser.getString("-html");
-			try {
-				if (seed != null && !seed.isBlank()) {
-					URI seedUri = URI.create(seed);
-					int crawlLimit = parser.hasFlag("-crawl")
-							? parser.getInteger("-crawl", 1)
-							: 1;
-					if (crawlLimit <= 0) {
-						crawlLimit = 1;
-					}
-					WebCrawler crawler = new WebCrawler(index, queue, crawlLimit);
-					crawler.crawl(seedUri);
-				} else {
-					System.out.println("No seed URI provided for web crawling.");
+			ThreadSafeInvertedIndex index = new ThreadSafeInvertedIndex();
+			queue = new WorkQueue(threads);
+			Searcher searcher = new ThreadedSearchProcessor(index, queue);
+
+			if (parser.hasFlag("-text") && parser.hasValue("-text")) {
+				Path path = parser.getPath("-text", Path.of(""));
+				try {
+					ThreadFileIndexer.indexPath(path, index, queue);
+				} catch (IOException e) {
+					System.out.println("Unable to index text file: " + path);
 				}
-			} catch (Exception e) {
-				System.out.println("Unable to crawl the web.");
 			}
-		}
 
-		if (parser.hasFlag("-query") && parser.hasValue("-query")) {
-			Path queryPath = parser.getPath("-query", Path.of(""));
-			try {
-				searcher.processFile(queryPath, partial);
-			} catch (IOException e) {
-				System.out.println("Unable to search on the input file.");
+			if (parser.hasFlag("-html")) {
+				String seed = parser.getString("-html");
+				try {
+					if (seed != null && !seed.isBlank()) {
+						URI seedUri = URI.create(seed);
+						int crawlLimit = parser.hasFlag("-crawl") ? parser.getInteger("-crawl", 1) : 1;
+						if (crawlLimit <= 0) {
+							crawlLimit = 1;
+						}
+						WebCrawler crawler = new WebCrawler(index, queue, crawlLimit);
+						crawler.crawl(seedUri);
+					} else {
+						System.out.println("No seed URI provided for web crawling.");
+					}
+				} catch (Exception e) {
+					System.out.println("Unable to crawl the web.");
+				}
 			}
-		}
 
-		if (parser.hasFlag("-counts")) {
-			Path countPath = parser.getPath("-counts", Path.of("counts.json"));
-			try {
-				index.countJson(countPath);
-			} catch (IOException e) {
-				System.out.println("Unable to write counts at path: " + countPath);
+			if (parser.hasFlag("-query") && parser.hasValue("-query")) {
+				Path queryPath = parser.getPath("-query", Path.of(""));
+				try {
+					searcher.processFile(queryPath, partial);
+				} catch (IOException e) {
+					System.out.println("Unable to search on the input file.");
+				}
 			}
-		}
 
-		if (parser.hasFlag("-index")) {
-			Path indexPath = parser.getPath("-index", Path.of("index.json"));
-			try {
-				index.indexJson(indexPath);
-			} catch (IOException e) {
-				System.out.println("Unable to write index at path: " + indexPath);
+			if (parser.hasFlag("-counts")) {
+				Path countPath = parser.getPath("-counts", Path.of("counts.json"));
+				try {
+					index.countJson(countPath);
+				} catch (IOException e) {
+					System.out.println("Unable to write counts at path: " + countPath);
+				}
 			}
-		}
 
-		if (parser.hasFlag("-results")) {
-			Path searchPath = parser.getPath("-results", Path.of("results.json"));
-			try {
-				searcher.searchJson(searchPath, partial);
-			} catch (IOException e) {
-				System.out.println("Unable to write to the output file.");
+			if (parser.hasFlag("-index")) {
+				Path indexPath = parser.getPath("-index", Path.of("index.json"));
+				try {
+					index.indexJson(indexPath);
+				} catch (IOException e) {
+					System.out.println("Unable to write index at path: " + indexPath);
+				}
 			}
-		}
 
-		queue.shutdown();
-		queue.join();
+			if (parser.hasFlag("-results")) {
+				Path searchPath = parser.getPath("-results", Path.of("results.json"));
+				try {
+					searcher.searchJson(searchPath, partial);
+				} catch (IOException e) {
+					System.out.println("Unable to write to the output file.");
+				}
+			}
 
-		if (serverPort > 0) {
-			try {
-				JettyServer server = new JettyServer(serverPort, index);
-				server.startAndJoin();
-			} catch (Exception e) {
-				System.out.println("Unable to start the web server.");
+			if (queue != null) {
+				queue.shutdown();
+				queue.join();
+				queue = null;
+			}
+
+			if (serverPort > 0) {
+				try {
+					JettyServer server = new JettyServer(serverPort, index);
+					server.startAndJoin();
+				} catch (Exception e) {
+					System.out.println("Unable to start the web server.");
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Unable to run the search engine.");
+		} finally {
+			if (queue != null) {
+				queue.shutdown();
+				queue.join();
 			}
 		}
 	}
