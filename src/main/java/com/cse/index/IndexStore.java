23e359f09fdd28a465eff82d5821606ed53f670d
john-la-usfca-98 <pkla@dons.usfca.edu>
2026-06-24 02:49:54 -0500

    feat(index): add IndexStore interface

---
+++ src/main/java/com/cse/index/IndexStore.java
@@ -0,0 +1,41 @@
+package com.cse.index;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Application-facing index API. Runtime implementation: {@code LuceneIndexStore}.
+ */
+public interface IndexStore extends AutoCloseable {
+
+	void open(Path indexDir) throws IOException;
+
+	void commit() throws IOException;
+
+	@Override
+	void close() throws IOException;
+
+	boolean isOpen();
+
+	Path indexDirectory();
+
+	void addDocument(IndexDocument doc) throws IOException;
+
+	void deleteDocument(String id) throws IOException;
+
+	long documentCount();
+
+	Set<String> listTerms();
+
+	Set<String> listLocations();
+
+	Set<String> locationsForTerm(String term);
+
+	List<SearchHit> search(SearchQuery query, SearchOptions options) throws IOException;
+
+	void exportJson(Path path) throws IOException;
+
+	void exportYaml(Path path) throws IOException;
+}
